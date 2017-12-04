3cd04f7d27e52bc7711b4edc2fb043a05572ab50
Greg Stanton Marra <greg.marra@gmail.com>
2017-12-03 22:18:07 -0800

    Add a couple demonstration modes

--- frc-leds/frc-leds.ino
+++ frc-leds/frc-leds.ino
@@ -14,6 +14,7 @@
  */
 
 #include <elapsedMillis.h>
+#include <FastLED.h>
 
 #define NUMBEROFMODES 3
 typedef enum {MODE_ZERO, MODE_ONE, MODE_TWO} led_mode;
@@ -31,7 +32,15 @@ int serialInputParams[MAXSERIALINPUTPARAMS];
 
 elapsedMillis blink_timer;
 
+#define NUM_LEDS 16
+#define LED_PIN 6
+
+CRGB leds[NUM_LEDS];
+
 void setup() {
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+  set_max_power_in_volts_and_milliamps(5, 500);
+  
   Serial.begin(9600);
 
   pinMode(LED_BUILTIN, OUTPUT);
@@ -41,6 +50,7 @@ void setup() {
 void loop() {
   serviceSerialInput();
   serviceMode();
+  FastLED.show();
 }
 
 void switchMode(int new_mode_index, int arguments[]) {
@@ -86,6 +96,12 @@ void doModeOne() {
     blink_timer = 0;
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   }
+
+  // from https://github.com/atuline/FastLED-Demos/blob/master/rainbow_beat/rainbow_beat.ino
+  uint8_t beatA = beatsin8(17, 0, 255);                        // Starting hue
+  uint8_t beatB = beatsin8(13, 0, 255);
+  fill_rainbow(leds, NUM_LEDS, (beatA+beatB)/2, 8);            // Use FastLED's fill_rainbow routine.
+  FastLED.setBrightness(serialInputParams[1]);
 }
 
 void doModeTwo() {
@@ -93,6 +109,19 @@ void doModeTwo() {
     blink_timer = 0;
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   }
+
+  // from https://github.com/atuline/FastLED-Demos/blob/master/dot_beat/dot_beat.ino
+  uint8_t bpm = 30;
+  uint8_t inner = beatsin8(bpm, NUM_LEDS/4, NUM_LEDS/4*3);    // Move 1/4 to 3/4
+  uint8_t outer = beatsin8(bpm, 0, NUM_LEDS-1);               // Move entire length
+  uint8_t middle = beatsin8(bpm, NUM_LEDS/3, NUM_LEDS/3*2);   // Move 1/3 to 2/3
+
+  leds[middle] = CRGB::Purple;
+  leds[inner] = CRGB::Blue;
+  leds[outer] = CRGB::Aqua;
+
+  nscale8(leds, NUM_LEDS, 224);                             // Fade the entire array. Or for just a few LED's, use  nscale8(&leds[2], 5, fadeval);
+
 }
 
 void serviceSerialInput() {
